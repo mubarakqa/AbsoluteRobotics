b21273a7a877e260fb85af6edc41836478b51dc2
Titilolami <titilolamia@yahoo.com>
2023-05-26 13:19:14 +0200

    Updated all modules

--- cypress/e2e/addtocart.cy.js
+++ cypress/e2e/addtocart.cy.js
@@ -7,16 +7,16 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 describe("Given I am on the homepage", function () {
 beforeEach(function () {
 cy.visit('/')
-});
-it("MYCART - I Should be able to add product to my cart", function () {
-  cy.get(addtocart.signupBtn).click()
-  cy.get(addtocart.usernameField).click()
-  cy.get(addtocart.usernameField).type('qlason@yopmail.com')
-  cy.get(addtocart.passwordField).click()
-  cy.get(addtocart.passwordField).type('TITILOLAMi@1')
-  cy.get(addtocart.loginBtn).click()
-  cy.wait(5000)
-  cy.get(addtocart.addBtn).click()
+// });
+// //it("MYCART - I Should be able to add product to my cart", function () {
+//  // cy.get(addtocart.signupBtn).click()
+//   cy.get(addtocart.usernameField).click()
+//   cy.get(addtocart.usernameField).type('qlason@yopmail.com')
+//   cy.get(addtocart.passwordField).click()
+//   cy.get(addtocart.passwordField).type('TITILOLAMi@1')
+//   cy.get(addtocart.loginBtn).click()
+//   cy.wait(5000)
+//   cy.get(addtocart.addBtn).click()
 })
 
 it("MYCART - I Should be able to view product on my cart", function () {
@@ -39,28 +39,17 @@ cy.get(addtocart.signupBtn).click()
   cy.wait(5000)
   cy.get(addtocart.cartitem).click()
   cy.get(addtocart.removeprodutbtn).click({force: true}) 
-})
 
-it("MYCART - I Should be able to increase my order quantity", function () {
- cy.get(addtocart.signupBtn).click()
- cy.get(addtocart.usernameField).click()
- cy.get(addtocart.usernameField).type('qlason@yopmail.com')
- cy.get(addtocart.passwordField).click()
-  cy.get(addtocart.passwordField).type('TITILOLAMi@1')
- cy.get(addtocart.loginBtn).click()
-  cy.get(addtocart.cartitem).click()
- cy.wait(5000)
-  cy.get(addtocart.plusbtn).click()
-})
-it("MYCART - I Should be able to descrease my order quantity", function () {
-  cy.get(addtocart.signupBtn).click()
-   cy.get(addtocart.usernameField).click()
-   cy.get(addtocart.usernameField).type('qlason@yopmail.com')
-   cy.get(addtocart.passwordField).click()
-   cy.get(addtocart.passwordField).type('TITILOLAMi@1')
-   cy.get(addtocart.loginBtn).click()
-   cy.get(addtocart.cartitem).click()
-   cy.wait(5000)
-   cy.get(addtocart.minusbtn).click()
+// })
+// it("MYCART - I Should be able to descrease my order quantity", function () {
+//   cy.get(addtocart.signupBtn).click()
+//    cy.get(addtocart.usernameField).click()
+//    cy.get(addtocart.usernameField).type('qlason@yopmail.com')
+//    cy.get(addtocart.passwordField).click()
+//    cy.get(addtocart.passwordField).type('TITILOLAMi@1')
+//    cy.get(addtocart.loginBtn).click()
+//    cy.get(addtocart.cartitem).click()
+//    cy.wait(5000)
+//    cy.get(addtocart.minusbtn).click()
  })
 })
